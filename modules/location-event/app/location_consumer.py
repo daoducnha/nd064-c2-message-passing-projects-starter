73ca145263b96b198a9019a3fc178c2578a6a607
nhadd <aoducnha29493@gmail.com>
2022-12-25 16:40:05 +0700

    Update location event

--- modules/location-event/app/location_consumer.py
+++ modules/location-event/app/location_consumer.py
@@ -1,9 +1,13 @@
+import os
 from kafka import KafkaConsumer
 import json
 from services import LocationService
 
 TOPIC_NAME = 'locations'
 KAFKA_SERVER = 'localhost:9092'
+
+# TOPIC_NAME = os.getenv('TOPIC_NAME')
+# KAFKA_SERVER = os.getenv('KAFKA_SERVER')
 
 consumer = KafkaConsumer(TOPIC_NAME, bootstrap_servers=KAFKA_SERVER)
 for message in consumer:
